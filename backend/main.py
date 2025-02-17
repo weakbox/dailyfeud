cc3cb0c1a454dec18ee714aaed3bdf7b6791d83c
Connor McLeod <connormcleod2000@icloud.com>
2025-02-17 12:24:11 -0400

    set up gpt-4o

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 from thefuzz import process
+from openai import OpenAI
+
+load_dotenv()
 
 class GuessRequest(BaseModel):
     text: str
@@ -20,9 +25,42 @@ ANSWER_SET = {
 ANSWER_SET_FLATTENED = {synonym: key for key, synonyms in ANSWER_SET.items() for synonym in synonyms}
 THRESHOLD = 80  # It's currently pretty generous. Especially when including one word in a multi-word answer (e.g. "coffee" -> "coffee table").
 
+def generate_answer_set(question: str):
+    prompt = f"Given the following question: {question}, provide a list of the 3-8 most common answers in the following format: {ANSWER_SET}. The answers should be sorted by most popular, similar to Family Feud."
+
+    client = OpenAI(
+        base_url="https://models.inference.ai.azure.com",
+        api_key=os.environ["OPENAI_API_KEY"],
+    )
+
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "",
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-4o",
+        temperature=1,
+        max_tokens=4096,
+        top_p=1
+    )
+
+    return response.choices[0].message.content
+
 @app.get("/")
 async def root():
-    return {"message": "Written and developed by Connor McLeod"}
+    test = generate_answer_set("Name A Job Where It Would Be Okay To Yell At Work.")
+    print(test)
+
+    return {
+        "message": "Hello! This is the DailyFeud backend.",
+        "creator": "Connor 'weakbox' McLeod"
+    }
 
 @app.post("/submit-guess/")
 async def submit_guess(guess: GuessRequest):
