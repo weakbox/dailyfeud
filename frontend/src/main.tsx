b4308e9f59c7dcf8e5db81172ec288bb8651e300
Connor McLeod <connormcleod2000@icloud.com>
2025-02-24 17:19:22 -0400

    basic setup for react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import GamePage from "./pages/GamePage.tsx";
+import LevelSelectPage from "./pages/LevelSelectPage.tsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <GamePage />,
+  },
+  {
+    path: "/level-select",
+    element: <LevelSelectPage />,
+  }
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
