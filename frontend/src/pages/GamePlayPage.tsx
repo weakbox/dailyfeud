8bb991fc6cdf8c149ae86e2a9da95aedaf41dc40
Connor McLeod <connormcleod2000@icloud.com>
2025-02-27 15:41:12 -0400

    coupled question and answer state together

--- frontend/src/pages/GamePlayPage.tsx
+++ frontend/src/pages/GamePlayPage.tsx
@@ -3,14 +3,27 @@ import { useParams, Link } from "react-router";
 import correct from "../assets/correct.mp3";
 
 const BASE_URL = "http://127.0.0.1:8000";
-const GET_QUESTION_URL = (id) => `${BASE_URL}/get-question-prompt/${id}`;
+const GET_QUESTION_URL = (id: number) =>
+  `${BASE_URL}/get-question-prompt/${id}`;
 const POST_GUESS_URL = `${BASE_URL}/submit-guess/`;
+
+type Question = {
+  prompt: string;
+  answers: Answer[];
+};
+
+type Answer = {
+  text: string;
+  value: number;
+};
 
 function GamePlayPage() {
   const { id } = useParams();
+  const [question, setQuestion] = useState<Question>({
+    prompt: "",
+    answers: [],
+  });
   const [guess, setGuess] = useState("");
-  const [question, setQuestion] = useState("");
-  const [answers, setAnswers] = useState(null);
 
   const audioRef = useRef(new Audio(correct));
 
@@ -27,8 +40,10 @@ function GamePlayPage() {
       try {
         const response = await fetch(GET_QUESTION_URL(id));
         const data = await response.json();
-        setQuestion(data.prompt);
-        setAnswers(new Array(data.count).fill(""));
+        setQuestion({
+          prompt: data.prompt,
+          answers: new Array(data.count).fill(""),
+        });
       } catch (error) {
         console.error("Error fetching question:", error);
       }
@@ -37,19 +52,18 @@ function GamePlayPage() {
     fetchQuestion();
   }, []);
 
-  // Play a sound when state of answers updates.
   useEffect(() => {
     playCorrect();
-  }, [answers]);
+  }, [question]);
 
   // Dynamically renders "count" number of answer boxes.
-  const renderBoxes = (count) =>
+  const renderBoxes = (count: number) =>
     Array.from({ length: count }, (_, i) => (
       <div
         key={i}
-        className={`rounded-md border-3 border-black px-4 py-4 text-center font-bold ${answers[i] ? "bg-green-300" : "bg-blue-400"}`}
+        className={`rounded-md border-3 border-black px-4 py-2 text-center font-bold ${question.answers[i].text ? "bg-green-300" : "bg-blue-400"}`}
       >
-        {answers[i] ? answers[i] : i + 1}
+        {question.answers[i].text ? question.answers[i].text : i + 1}
       </div>
     ));
 
@@ -75,11 +89,14 @@ function GamePlayPage() {
       console.log("Guess submitted successfully:", result);
 
       if (result.correct) {
-        setAnswers((prevAnswers) =>
-          prevAnswers.map((a, i) =>
-            i + 1 === result.position ? result.answer.toUpperCase() : a,
-          ),
-        );
+        setQuestion((prevQuestion) => ({
+          ...prevQuestion,
+          answers: prevQuestion.answers.map((a, i) => ({
+            text:
+              i + 1 === result.position ? result.answer.toUpperCase() : a.text,
+            value: i + 1 === result.position ? result.answer.value : a.value,
+          })),
+        }));
       }
     } catch (error) {
       console.error("Failed to submit guess:", error);
@@ -90,12 +107,13 @@ function GamePlayPage() {
 
   return (
     <div className="flex w-full flex-col items-center gap-4 p-2 text-center">
-      <h1 className="w-full rounded-md border-3 border-black bg-blue-400 px-4 py-4 text-center text-4xl font-bold">
-        {question.toUpperCase()}
+      <h1 className="w-full rounded-md border-3 border-black bg-blue-400 px-4 py-2 text-center text-2xl font-bold">
+        {question.prompt.toUpperCase()}
       </h1>
 
-      <div className="grid w-full grid-flow-col grid-cols-1 grid-rows-8 gap-2 sm:grid-cols-2 sm:grid-rows-4">
-        {answers && renderBoxes(answers.length)}
+      {/* These classes are implemented pretty badly I think. */}
+      <div className="grid w-full auto-rows-auto grid-cols-1 gap-2 sm:grid-flow-col sm:grid-cols-2 sm:grid-rows-4">
+        {renderBoxes(question.answers.length)}
       </div>
 
       <form onSubmit={handleGuess} className="flex w-full flex-row gap-2">
@@ -104,18 +122,19 @@ function GamePlayPage() {
           value={guess}
           onChange={(e) => setGuess(e.target.value.toUpperCase())}
           placeholder="ENTER A GUESS..."
-          className="w-4/5 rounded-md border-3 border-black bg-yellow-300 px-4 py-4 font-bold"
+          className="w-4/5 rounded-md border-3 border-black bg-yellow-300 px-4 py-2 font-bold"
         />
         <input
           type="submit"
-          value="ENTER"
-          className="w-1/5 rounded-md border-3 border-black bg-pink-300 px-4 py-4 font-bold overflow-ellipsis"
+          value="GUESS"
+          className="w-1/5 rounded-md border-3 border-black bg-pink-300 px-4 py-2 font-bold overflow-ellipsis"
+          disabled={!guess.trim()}
         />
       </form>
 
       <Link
         to="/archive"
-        className="w-full rounded-md border-3 border-black bg-pink-300 px-4 py-4 font-bold"
+        className="w-full rounded-md border-3 border-black bg-pink-300 px-4 py-2 font-bold"
       >
         QUESTION ARCHIVE
       </Link>
