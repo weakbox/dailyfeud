fa2170e3ed89d5fefc70871f4f4f48cc4da228f2
Connor McLeod <connormcleod2000@icloud.com>
2025-03-21 20:08:24 -0300

    refactored database interactions for PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -9,14 +9,7 @@ def get_connection():
     """
     Get a connection to the database and enable foreign key constraints.
     """
-    conn = psycopg.connect(DATABASE_URL)
-
-    # Just for debugging right now!
-    with conn.cursor() as cur:
-        cur.execute("SELECT version()")
-        print(cur.fetchone())
-
-    return conn
+    return psycopg.connect(DATABASE_URL)
 
 def initialize_database() -> None:
     """
@@ -60,46 +53,45 @@ def store_question(question: QuestionModel) -> None:
                 (question.question,)).fetchone()[0]
 
             for answer, answer_model in question.answers.items():
+                position = answer_model.position
+                synonyms = answer_model.synonyms
+                points = answer_model.value
+
                 cur.execute("""
                     INSERT INTO answers (question_id, position, answer, synonyms, points)
                     VALUES (%s, %s, %s, %s, %s);
                     """, 
-                    (question_id, answer_model.position, answer, answer_model.synonyms, answer_model.value,))
-
-
+                    (question_id, position, answer, synonyms, points,))
 
 def retrieve_latest_question_id() -> Optional[str]:
     """
     Retrieve the latest question ID from the questions table.
     """
-    with get_connection() as con:
-        cur = con.cursor()
-        cur.execute("""
-            SELECT id FROM questions 
-            ORDER BY id DESC
-            LIMIT 1
-        """)
-        result = cur.fetchone()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            latest_id = cur.execute("""
+                SELECT id FROM questions 
+                ORDER BY id DESC
+                LIMIT 1
+                """).fetchone()[0]
     
-    return str(result[0]) if result else None
+    return str(latest_id) if latest_id else None
 
+# Untested!
 def retrieve_question(id: int) -> QuestionModel:
     """
-    Retrieve a question, answers, and synonyms from the database.
+    Retrieve a specific question and its associated answers from the database.
     """
-    with get_connection() as con:
-        cur = con.cursor()
-        cur.execute("""
-            SELECT question, answer, synonym, points, position
-            FROM questions 
-            JOIN answers
-            ON questions.id = answers.question_id
-            JOIN synonyms 
-            ON answers.id = synonyms.answer_id 
-            WHERE question_id = ?
-            """, (id,))
-        
-        results = cur.fetchall()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            results = cur.execute("""
+                SELECT question, position, answer, synonyms, points
+                FROM questions 
+                JOIN answers
+                ON questions.id = answers.question_id
+                WHERE question_id = %s;
+                """, 
+                (id,)).fetchall()
     
     if not results:
         raise ValueError(f"Question with ID {id} not found.")
@@ -107,10 +99,8 @@ def retrieve_question(id: int) -> QuestionModel:
     question = results[0][0]
     answers = {}
 
-    for _, answer, synonym, points, position in results:
-        if answer not in answers:
-            answers[answer] = AnswerModel(position=position, value=points, synonyms=[answer])
-        answers[answer].synonyms.append(synonym)
+    for _, position, answer, synonyms, points in results:
+        answers[answer] = AnswerModel(position=position, value=points, synonyms=synonyms)
 
     return QuestionModel(question=question, answers=answers)
 
@@ -118,16 +108,17 @@ def retrieve_question_prompt(id: int) -> dict[str, int | str]:
     """
     Retrieve a question prompt and number of answers from the database based off of the question's ID.
     """
-    with get_connection() as con:
-        cur = con.cursor()
-        cur.execute("""
-                    SELECT question, COUNT(*)
-                    FROM questions 
-                    JOIN answers
-                    ON questions.id = answers.question_id
-                    WHERE question_id = ?
-                    """, (id,))
-        result = cur.fetchone()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            result = cur.execute("""
+                SELECT question, COUNT(answers.id)
+                FROM questions 
+                LEFT JOIN answers
+                ON questions.id = answers.question_id
+                WHERE questions.id = %s
+                GROUP BY questions.id
+                """, 
+                (id,)).fetchone()
 
     if result is None or result[0] is None:
         raise ValueError(f"Question with ID {id} not found.")
@@ -141,12 +132,12 @@ def retrieve_all_question_prompts() -> List[Dict[str, int | str]]:
     """
     Retrieve all question prompts from the database.
     """
-    con = get_connection()
-    cur = con.cursor()
-
-    cur.execute("SELECT id, question FROM questions")
-    result = cur.fetchall()
-    con.close()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            result = cur.execute("""
+                SELECT id, question 
+                FROM questions
+                """).fetchall()
 
     prompts = []
     for row in result:
@@ -161,12 +152,14 @@ def retrieve_all_answers(id: int):
     """
     Retrieve all answers for a given question ID from the database.
     """
-    con = get_connection()
-    cur = con.cursor()
-
-    cur.execute("SELECT answer, points, position FROM answers WHERE question_id = ?", (id,))
-    result = cur.fetchall()
-    con.close()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            result = cur.execute("""
+                SELECT answer, points, position 
+                FROM answers 
+                WHERE question_id = %s
+                """,
+                (id,)).fetchall()
 
     answers = []
     for row in result:
